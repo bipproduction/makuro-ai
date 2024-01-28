c3b2d83e69e340bba1a1a081dfa85a4eccb12116
bipproduction <bip.production.js@gmail.com>
2024-01-28 12:35:15 +0800

    auto push

--- xcoba.js
+++ xcoba.js
@@ -15,27 +15,3 @@ fetch('http://localhost:3000/ask', {
         console.log(data.toString())
     })
 })
-
-
-// fetch('https://makuro-ai.wibudev.com/ask', {
-//     method: "POST",
-//     body: JSON.stringify({
-//         q: "siapa tom lembong",
-//         n: false
-//     }),
-//     headers: {
-//         "Content-Type": "application/json"
-//     }
-// }).then(async (v) => {
-//     const body = v.body
-//     body.on("data", (data) => {
-//         console.log(data.toString())
-//     })
-// })
-
-
-// const a = require('./xcoba.json')
-// const _ = require('lodash')
-// const fd = _.flattenDeep(a).find((v) => `${v}`.length > 50)
-// const fd2 = _.flatMapDeep(JSON.parse(fd))
-// console.log(fd2.find(v => `${v}`.length > 50))
